e310408db4f850077bbb6c5aa5c23ce0bb2c078a
Luu Duc Hoang <hoangld4@vng.com.vn>
2022-04-05 17:43:11 +0700

    test run deeplink kyc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <>
+
         <Script src="https://h5.zdn.vn/jssdk/1.7.18/lib.js"></Script>
         <Component {...pageProps} />
       </>
